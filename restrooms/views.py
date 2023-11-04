745a3de269105b2c74098ce7268c440ef0030496
Krishna <kshah1067530@gmail.com>
2023-11-04 08:36:39 -0500

    [Add] -> Working with restoom page and data

--- restrooms/views.py
+++ restrooms/views.py
@@ -1,58 +1,58 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from geopy.distance import geodesic
 from django.core.paginator import Paginator
 import json
 
+
+def save_data(request):
+    request.session['restrooms'] = json.loads(request.body)
+    print('Data is stored in the session.')
+    return JsonResponse({'status': 'success'})
+
+
 def index(request):
-    with open('data.json', 'r') as f:
-        data = json.load(f)
+    # del request.session['restrooms']
+    if 'restrooms' in request.session:
         restrooms = []
-        addressSet = set()
+        data = request.session['restrooms']
 
-        if request.COOKIES.get('latitude', None) and request.COOKIES.get('longitude', None):
-            origin = (request.COOKIES.get('latitude'), request.COOKIES.get('longitude'))
-        else:
-            # Default UNT Dallas Location
-            origin = ('30.28575', '-97.72920')
-        print(origin)
-        for loc in data['results']:
-            if loc["formatted_address"] not in list(addressSet):
-                dest = (loc['geometry']['location']['lat'], loc['geometry']['location']['lng'])
+        print(data['nearbyRestrooms'][0])
+        # return JsonResponse({'status': 'success', 'data': data})
+        for place in data['nearbyRestrooms']:
+            origin = (data['currentLocation']['lat'], data['currentLocation']['lng'])
+            image_url = f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference={place["placeId"]}&key=AIzaSyCxEhJzSEW4WQOINMyTdUblD0w5WRd-n4w' if place['placeId'] else None
+            name = place['name']
+            address = place['address']
+            rating = place['rating'] if place['rating'] else -1
+            lat = place['lat']
+            lng = place['lng']
+            dest = (lat, lng)
+            distance = geodesic(origin, dest).miles
 
-                if 'photos' not in loc:
-                    image_url = None
-                    restrooms.append({'name': loc["name"], 'address': loc["formatted_address"], 'rating': loc['rating'],
-                                      'lat': loc['geometry']['location']['lat'],
-                                      'lng': loc['geometry']['location']['lng'], 'image_url': image_url, 'distance': geodesic(origin, dest).miles})
+            restrooms.append({'name': name, 'address': address, 'rating': rating, 'lat': lat, 'lng': lng, 'distance': distance})
+
+        if 'sortby' in request.GET:  # or 'sort' in request.GET:
+            if request.GET.get('sortby') == 'rating':
+                sort = request.GET.get('sort')
+                if sort == 'asc':
+                    restrooms = sorted(restrooms, key=lambda d: d['rating'])
                 else:
-                    photo_ref = loc["photos"][0]["photo_reference"]
-                    image_url = f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference={photo_ref}&key=AIzaSyCxEhJzSEW4WQOINMyTdUblD0w5WRd-n4w'
+                    restrooms = sorted(restrooms, key=lambda d: d['rating'], reverse=True)
+            elif request.GET.get('sortby') == 'distance':
+                sort = request.GET.get('sort')
+                if sort == 'asc':
+                    restrooms = sorted(restrooms, key=lambda d: d['distance'])
+                else:
+                    restrooms = sorted(restrooms, key=lambda d: d['distance'], reverse=True)
 
-                    restrooms.append({'name': loc["name"], 'address': loc["formatted_address"], 'rating': loc['rating'], 'lat': loc['geometry']['location']['lat'], 'lng': loc['geometry']['location']['lng'], 'image_url': image_url, 'distance': geodesic(origin, dest).miles})
-            addressSet.add(loc["formatted_address"])
-
-    if 'sortby' in request.GET: # or 'sort' in request.GET:
-        if request.GET.get('sortby') == 'rating':
-            sort = request.GET.get('sort')
-            if sort == 'asc':
-                restrooms = sorted(restrooms, key=lambda d: d['rating'])
-            else:
-                restrooms = sorted(restrooms, key=lambda d: d['rating'], reverse=True)
-        elif request.GET.get('sortby') == 'distance':
-            sort = request.GET.get('sort')
-            if sort == 'asc':
-                restrooms = sorted(restrooms, key=lambda d: d['distance'])
-            else:
-                restrooms = sorted(restrooms, key=lambda d: d['distance'], reverse=True)
-
-    per_page = 3
-    paginator = Paginator(restrooms, per_page)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    if not page_number:
-        page_number = 1
-    restrooms = paginator.page(page_number)
-    # response = render(request, 'restrooms.html', {'restrooms': restrooms, 'page_obj': page_obj})
-    # response.set_cookie(keu='', value=)
-    # response.set_cookie(keu='', value=)
-    return render(request, 'restrooms.html', {'restrooms': restrooms, 'page_obj': page_obj})
+        per_page = 3
+        paginator = Paginator(restrooms, per_page)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        if not page_number:
+            page_number = 1
+        restrooms = paginator.page(page_number)
+        return render(request, 'restrooms.html', {'restrooms': restrooms, 'page_obj': page_obj})
+    else:
+        return render(request, 'restrooms.html')
