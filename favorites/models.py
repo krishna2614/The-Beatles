7aad36ef4c862bce1c606480c03c036d10ba1c80
Krishna <kshah1067530@gmail.com>
2023-11-29 14:48:48 -0600

    [Change] -> removed extra field for favorites

--- favorites/models.py
+++ favorites/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from accounts.models import User
-import jsonfield
 
 
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    restroom_details = jsonfield.JSONField()
