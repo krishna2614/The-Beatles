385fec9159b80ec968b17c22a001c90b04937cc1
Krishna <kshah1067530@gmail.com>
2023-11-29 18:58:08 -0600

    [Add & Change] Added Favourite feature and area of focus& Changed About page

--- areaOfFocus/urls.py
+++ areaOfFocus/urls.py
@@ -1,6 +1,7 @@
+from . import views
 from django.urls import path
-from .views import index
 
 urlpatterns = [
-    path('', index, name='area_of_focus'),
+    path('', views.PostList.as_view(), name='area_of_focus'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- areaOfFocus/views.py
+++ areaOfFocus/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'areaoffocus.html'
 
-
-def index(request):
-    return render(request, 'areaoffocus.html')
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'article.html'
