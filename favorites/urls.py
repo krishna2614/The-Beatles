de0c265feb71aa2dca20b8034c5b5403678d7514
Krishna <kshah1067530@gmail.com>
2023-11-30 13:39:31 -0600

    [ADD] -> Favourite Page & Distance and Rating Filter

--- favorites/urls.py
+++ favorites/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('update-favorite/', views.update_favorite, name='update-favorite'),
     path('get-user-favorites/', views.get_user_favorites, name='get-user-favorites'),
+    path('favorites/', views.show_page, name='fav_page'),
+
 ]
--- favorites/views.py
+++ favorites/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from django.shortcuts import render, redirect
+
 from .models import Favorite
 import json
 
@@ -17,13 +19,27 @@ def update_favorite(request):
         data = json.loads(request.body)
         restroom = data['restroom']
         is_favorite = data['favorite']
+        from_fav = data['fromFav']
 
         if is_favorite:
             # Create favorite
             Favorite.objects.create(user=user, restroom=restroom)
         else:
             # Remove from favorites
-            restroom['isFav'] = False if restroom['isFav'] else True
+            if from_fav:
+                restroom['isFav'] = True if restroom['isFav'] else False
+            else:
+                restroom['isFav'] = False if restroom['isFav'] else True
             Favorite.objects.filter(user=user, restroom=restroom).delete()
         return JsonResponse({'status': 'success'})
     return JsonResponse({'status': 'invalid request'}, status=400)
+
+
+def show_page(request):
+    if request.user.is_authenticated:
+        favorites = Favorite.objects.filter(user=request.user)
+        favorite_restrooms = [favorite.restroom for favorite in favorites]
+
+        print(favorite_restrooms)
+        return render(request, 'restrooms.html', {'restrooms': favorite_restrooms})
+    return redirect('index')
